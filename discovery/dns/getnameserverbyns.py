594b7e4e4126201ea0352ba22450189b0ee9427e
jekil <alessandro@tanasi.it = Alessandro Tanasi alessandro@tanasi.it>
2008-11-08 11:12:44 +0000

    Changed license to private license

--- discovery/dns/getnameserverbyns.py
+++ discovery/dns/getnameserverbyns.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python
+#
+#   hostmap
+#
+#   Author:
+#    Alessandro `jekil` Tanasi <alessandro@tanasi.it>
+#
+#   License:
+#    This program is private software; you can't redistribute it and/or modify
+#    it. All copies, included printed copies, are unauthorized.
+#    
+#    If you need a copy of this software you must ask for it writing an
+#    email to Alessandro `jekil` Tanasi <alessandro@tanasi.it>
+
+
+
 from twisted.names import client
 import lib.core.outputDeflector as log
 
--- discovery/web/__init__.py
+++ discovery/web/__init__.py
@@ -1,1 +1,13 @@
-
+#!/usr/bin/env python
+#
+#   hostmap
+#
+#   Author:
+#    Alessandro `jekil` Tanasi <alessandro@tanasi.it>
+#
+#   License:
+#    This program is private software; you can't redistribute it and/or modify
+#    it. All copies, included printed copies, are unauthorized.
+#    
+#    If you need a copy of this software you must ask for it writing an
+#    email to Alessandro `jekil` Tanasi <alessandro@tanasi.it>
