465843abc7eac902df7ec16865a156809ac7eeeb
jekil <alessandro@tanasi.it = Alessandro Tanasi alessandro@tanasi.it>
2008-11-15 00:42:40 +0000

    Better docstring

--- lib/settings.py
+++ lib/settings.py
@@ -12,15 +12,20 @@
 #    If you need a copy of this software you must ask for it writing an
 #    email to Alessandro `jekil` Tanasi <alessandro@tanasi.it>
 
-
+"""
+Configuration file. Some environment variables can be changed here.
+@license: Private licensing
+@author: Alessandro Tanasi
+@contact: alessandro@tanasi.it
+"""
 
 # Version specific
 VERSION = "0.1 private"
 CODENAME = "ghironda"
 
 # Dictionaries settings
-HOSTLIST = "./dictionaries/hostnames.txt"
-HOSTLISTFULL = "./dictionaries/hostnames-full.txt"
+HOSTLIST = "dictionaries/hostnames.txt"
+HOSTLISTFULL = "dictionaries/hostnames-full.txt"
 
 # Plugins settings
-PLUGINDIR = "./discovery"
+PLUGINDIR = "discovery"
