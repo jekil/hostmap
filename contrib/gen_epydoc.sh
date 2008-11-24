de26451556248f678b4b95412b9b188c2bb6775d
jekil <alessandro@tanasi.it = Alessandro Tanasi alessandro@tanasi.it>
2008-11-24 23:17:14 +0000

    Moved epydoc generation script

--- contrib/gen_epydoc.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-
-epydoc --html -o doc/epydoc --name hostmap --url http://hostmap.sourceforge.net --graph all --css white -v ../lib/
---
+++ doc/gen_epydoc.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+
+epydoc --html -o epydoc --name hostmap --url http://hostmap.sourceforge.net --graph all --css white -v ../lib/
