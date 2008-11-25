ddd91bb11a21e562d3224d82751807ea27dc7c0c
jekil <alessandro@tanasi.it = Alessandro Tanasi alessandro@tanasi.it>
2008-11-25 23:50:10 +0000

    Refactored

--- lib/core/discovery/hostDiscovery.py
+++ lib/core/discovery/hostDiscovery.py
@@ -15,8 +15,8 @@
 
 
 import lib.intel as intel
-import lib.core.outputDeflector as log
-import lib.core.configuration as configuration
+from lib.core.outputDeflector import *
+from lib.core.configuration import *
 import lib.core.controllers.jobController as jobController
 from lib.common import *
 
@@ -25,7 +25,9 @@ from lib.common import *
 class hostMap:
     """ 
     Host Discovery
-    @author: Alessandro Tanasi
+    @author:       Alessandro Tanasi
+    @license:      Private software
+    @contact:      alessandro@tanasi.it
     """
     
     
@@ -54,10 +56,10 @@ class hostMap:
         self.jobControl = jobController.jobs(debug=True)
         
         # Create a host intelligence model 
-        self.host = intel.Host(self,  target)
+        self.host = intel.Host(self, target)
         
         if self.debug:
-            log.out.debug("Created new HostDiscovery for %s" % self.name,  time=True, tag=self.tag)
+            log.debug("Created new HostDiscovery for %s" % self.name, time=True,tag=self.tag)
 
 
 
@@ -68,6 +70,7 @@ class hostMap:
         
         return self.name
 
+    name = property(getName, _)
 
 
     def job(self, job, status):
@@ -77,8 +80,9 @@ class hostMap:
         @param status: status of the plugin
         """
         
-        self.jobControl.alter(job,  status)
+        self.jobControl.alter(job, status)
         
+        # TODO: move this
         # Check if all jobs are done, so Host Discovery is finished
         if self.jobControl.check():
             self.stop()
@@ -90,7 +94,7 @@ class hostMap:
         Starts Host Discovery
         """
         
-        if self.debug: log.out.debug("Host discovery started", time=True, tag=self.tag)
+        if self.debug: log.debug("Host discovery started", time=True, tag=self.tag)
          
         # Start discovery
         self.notifyIp(self.name)
@@ -103,14 +107,16 @@ class hostMap:
         Stops Host Discovery
         """
         
-        if self.debug: log.out.debug("Host discovery stopped", time=True, tag=self.tag)
+        if self.debug: log.debug("Host discovery stopped", time=True, tag=self.tag)
         self.host.status()
 
 
 
     def notifyIp(self, ip):
         """
-        A new ip address has been detected, run all needed plugins
+        A new ip address has been detected, run all needed plugins.
+        Steps:
+        * Run all checks that get a ip as input
         @params ip: ip address
         """
         
@@ -121,12 +127,16 @@ class hostMap:
     def notifyDomain(self, domain):
         """
         A new domain is detected, run all needed plugins
+        Steps:
+        * Check if the domain ip address is the target ip
+        * Run all checks that get a domain as input
+        * Get the nameserver for that domain
         @params domain: name of found domain
         """
         
         # Check if this domain has been already discovered
         if self.host.addDomain(domain):
-            log.out.info("Found new domain: %s" % domain, time=True, tag=self.tag)
+            log.info("Found new domain: %s" % domain, time=True, tag=self.tag)
             self.pluginControl.runByDomain(self, domain)
         # Get nameservers
         #if not self.conf.OnlyPassive:
@@ -136,13 +146,17 @@ class hostMap:
 
     def notifyNameserver(self, nameserver):
         """
-        A new nameserver is detected, run all needed plugins
+        A new nameserver is detected, run all needed pluginsdomain
+        Steps:
+        * Check if the nameserver ip address is the target ip
+        * Run all checks that get a nameserver as input
+        * Try a zone transfer
         @params nameserver: nameserver found
         """
         
         # Check if this domain has been already discovered
         if self.host.addNameserver(domain):
-            log.out.info("Found new nameserver: %s" % domain,  time=True, tag=self.tag)
+            log.info("Found new nameserver: %s" % domain, time=True, tag=self.tag)
             self.pluginControl.runByNameserver(self, nameserver)
         # Check if a name server is the target
         #if not self.conf.OnlyPassive:
@@ -157,16 +171,21 @@ class hostMap:
     def notifyHost(self, fqdn):
         """
         A new virtual host has been enumerated
+        Steps:
+        * Check if the host ip address is the target ip
+        * Run all checks that get a hostname as input
+        * Parse the domain name and notify a new domain (the hostname is also a domain name)
         @params fqdn: fully qualified domain name of enumerated virtual host
         """
         
         # Check if this virtual host has been already discovered
         if self.host.addHost(fqdn):
             # TODO: host or virtual host?
-            log.out.info("Found new host: %s" % fqdn,  time=True, tag=self.tag)
+            log.info("Found new host: %s" % fqdn, time=True,tag=self.tag)
             self.pluginControl.runByHostname(self, fqdn)
             
             # Grep domain and notify it
+            # todo 3 *
             domain = parseDomain(fqdn)
             self.notifyDomain(domain)
         
