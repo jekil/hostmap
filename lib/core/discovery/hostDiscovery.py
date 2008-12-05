5e070d4b4b7ebb67f1411b03240b8a7c06dcf52a
jekil <alessandro@tanasi.it = Alessandro Tanasi alessandro@tanasi.it>
2008-12-05 21:03:12 +0000

    Fixed import bug

--- lib/core/discovery/hostDiscovery.py
+++ lib/core/discovery/hostDiscovery.py
@@ -15,7 +15,7 @@
 
 
 import socket
-import lib.discovery.hostIntel as intel
+import lib.core.discovery.hostIntel as intel
 from lib.output.outputDeflector import log
 from lib.core.configuration import conf
 from lib.core.controllers.hmException import *
