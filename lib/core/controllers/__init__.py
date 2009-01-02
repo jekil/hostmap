9f0e8cfd5d395977f995a488a2188d48f9ed7a1d
jekil <alessandro@tanasi.it = Alessandro Tanasi alessandro@tanasi.it>
2009-01-02 23:47:51 +0000

    License fix

--- lib/core/controllers/__init__.py
+++ lib/core/controllers/__init__.py
@@ -20,4 +20,4 @@
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#    along with hostmap.  If not, see <http://www.gnu.org/licenses/>.
