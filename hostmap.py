643bedddfc31b75d2740fc383203cb65e5751dc4
jekil <alessandro@tanasi.it = Alessandro Tanasi alessandro@tanasi.it>
2009-01-23 21:07:37 +0000

    Refactored, hostmap as a class

--- hostmap.py
+++ hostmap.py
@@ -34,57 +34,70 @@ import lib.core.controllers.engineController as engine
 
 
 
-def main():
-    """ 
-    Start of the hostmap world. Main workflow
+class HostMap:
     """
-    
-    # Show banner, credits and stuff
-    showCredits()
-    
-    try:       
-        # Parse command line
-        options.parseArgs()
-        
-        # Preventive dependency check
-        deps.check()
-        
-        # Start hostmapping
-        engine.en.start()
-        
-    except hmImportException, e:
-        print
-        print "Execution aborted, missing dependencies!"
-        print e
-        
-    except hmOptionException, e:
-        print
-        print "Execution aborted, missing option value!"
-        print e
-        
-    except hmFileException, e:
-        print 
-        print "Execution aborted, file or directory not found!"
-        print e
-        
-    except Exception:
-        print
-        print "Unhandled exception. Please report this bug sending an email to alessandro@tanasi.it attaching the following text:"
-        traceback.print_exc()
+    Hostmap
+    @license: GNU Public License version 3
+    @author: Alessandro Tanasi
+    @contact: alessandro@tanasi.it
+    """
 
 
 
-def showCredits():
-    """
-    Show banner and credits
-    """
-    
-    log.info("hostmap %s codename %s" % (VERSION, CODENAME))
-    log.info("Coded by Alessandro `jekil` Tanasi <alessandro@tanasi.it>")
-    log.info("")
+    def start(self):
+        """ 
+        Start of the hostmap world. Main workflow
+        """
+        
+        # Show banner, credits and stuff
+        self.credits()
+        
+        try:       
+            # Parse command line
+            options.parseArgs()
+            
+            # Preventive dependency check
+            deps.check()
+            
+            # Start hostmapping
+            engine.en.start()
+            
+        except hmImportException, e:
+            print
+            print "Execution aborted, missing dependencies!"
+            print e
+            
+        except hmOptionException, e:
+            print
+            print "Execution aborted, missing option value!"
+            print e
+            
+        except hmFileException, e:
+            print 
+            print "Execution aborted, file or directory not found!"
+            print e
+            
+        except KeyboardInterrupt:
+            print "Execution aborted"
+            
+        except Exception:
+            print
+            print "Unhandled exception. Please report this bug sending an email to alessandro@tanasi.it attaching the following text:"
+            traceback.print_exc()
+
+
+
+    def credits(self):
+        """
+        Show banner and credits
+        """
+        
+        log.info("hostmap %s codename %s" % (VERSION, CODENAME))
+        log.info("Coded by Alessandro `jekil` Tanasi <alessandro@tanasi.it>")
+        log.info("")
     
 
 
 # Main
 if __name__ == "__main__":
-    main()
+    HostMap().start()
