ca7be40922279b7b1d9b70bb66fa66490eb91678
jekil <alessandro@tanasi.it = Alessandro Tanasi alessandro@tanasi.it>
2009-05-29 18:08:44 +0000

    Changed header

--- discovery/dns/dnsaxfrzonetransfer.py
+++ discovery/dns/dnsaxfrzonetransfer.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
-#
-#   hostmap
-#
-#   Author:
-#    Alessandro `jekil` Tanasi <alessandro@tanasi.it>
-#
-#   License:
-#
-#    This file is part of hostmap.
-#
-#    hostmap is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    hostmap is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with hostmap.  If not, see <http://www.gnu.org/licenses/>.>
+"""
+hostmap
+
+Author:
+Alessandro `jekil` Tanasi <alessandro@tanasi.it>
+
+License:
+
+This file is part of hostmap.
+
+hostmap is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+hostmap is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with hostmap.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 
 
