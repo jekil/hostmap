dedd2d89a0decfbd6846a64f437ea843e221753c
jekil <alessandro@tanasi.it = Alessandro Tanasi alessandro@tanasi.it>
2009-10-02 21:18:01 +0000

    Changed release to 0.1.1 codename tapioca

--- lib/settings.py
+++ lib/settings.py
@@ -39,7 +39,7 @@ from lib.core.hmException import hmOptionException
 from lib.core.configuration import conf
 
 # Version specific
-VERSION = "0.2"
+VERSION = "0.1.1"
 CODENAME = "tapioca"
 
 # Dictionaries settings
