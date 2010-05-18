726d32c22b01b432524c928357de3298558d4dc5
jekil <alessandro@tanasi.it = Alessandro Tanasi alessandro@tanasi.it>
2010-05-18 10:21:31 +0000

    Added an adaptive preventive check (Refs: #29)

--- discovery/dns/bruteforcebydomain.rb
+++ discovery/dns/bruteforcebydomain.rb
@@ -16,6 +16,7 @@ PlugMan.define :bruteforcebydomain do
 
   def run(domain, opts = {})
     @hosts = Set.new
+    @counter = 0
 
     # Configuration check
     if ! opts['dnsbruteforce']
@@ -30,29 +31,17 @@ PlugMan.define :bruteforcebydomain do
     # Initialization
     if opts['dns']
       dns = opts['dns'].gsub(/\s/, '').split(',')
-      res = Net::DNS::Resolver.new(:nameserver => dns)
+      @res = Net::DNS::Resolver.new(:nameserver => dns)
     else
-      res = Net::DNS::Resolver.new
+      @res = Net::DNS::Resolver.new
     end
 
     # Silence net-dns logger
-    res.log_level = Net::DNS::UNKNOWN
+    @res.log_level = Net::DNS::UNKNOWN
     
     # False positives or wildcard domain preventive check with random query
-    10.times do
-      test = (0...8).map{65.+(rand(25)).chr}.join 
-      begin
-        res.query("#{test}.#{domain}").answer.each do |rr|
-          if rr.class == Net::DNS::RR::A
-            if rr.address==IPAddr.new(opts['target'])
-              $LOG.warn "Detected a wildward domain: #{domain}"
-              return @hosts
-            end
-          end
-        end
-      rescue Exception
-        return @hosts
-      end
+    3.times do
+      return @hosts if prev_check(domain)
     end
     
     # Load brute force names list
@@ -66,9 +55,6 @@ PlugMan.define :bruteforcebydomain do
     if ! $HOSTDICT
       $HOSTDICT = File.open(file, "r").read
     end
-
-    threads = []
-    counter = 0
     
     # Brute force
     $HOSTDICT.each("\n") do |host|
@@ -82,11 +68,14 @@ PlugMan.define :bruteforcebydomain do
 
       # Resolve
       begin
-        res.query("#{host}.#{domain}").answer.each do |rr|
+        @res.query("#{host}.#{domain}").answer.each do |rr|
           # TODO: add this and report without check_host
           if rr.class == Net::DNS::RR::A
             if rr.address==IPAddr.new(opts['target'])
               @hosts << { :hostname => "#{host}.#{domain}" }
+              return Set.new if count(domain, true)
+            else
+              count(domain, false)
             end
           end
         end
@@ -100,5 +89,37 @@ PlugMan.define :bruteforcebydomain do
 
   def timeout
     return @hosts
+  end
+
+  #
+  # Preventive check to avoid false positives.
+  #
+  def prev_check(domain)
+    test = (0...8).map{65.+(rand(25)).chr}.join
+    begin
+      @res.query("#{test}.#{domain}").answer.each do |rr|
+        if rr.class == Net::DNS::RR::A
+          if rr.address==IPAddr.new(opts['target'])
+            $LOG.warn "Detected a wildcard domain: #{domain}"
+            return true
+          end
+        end
+      end
+    rescue Exception
+      return true
+    end
+    return false
+  end
+
+  def count(domain, status)
+    if status
+      @counter = @counter + 1
+      if @counter >= 5
+        return true if prev_check(domain)
+      end
+    else
+      @counter = 0
+      return false
+    end
   end
 end
