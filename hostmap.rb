4a15aa2bfc48fad62e2fd1414b32342cda13db24
jekil <alessandro@tanasi.it>
2012-03-19 21:36:07 +0100

    Refactored net-dns lib path

--- hostmap.rb
+++ hostmap.rb
@@ -4,7 +4,7 @@
 base = __FILE__
 $:.unshift(File.join(File.expand_path(File.dirname(base)), File.join('lib', 'hostmap')))
 $:.unshift(File.join(File.expand_path(File.dirname(base)), 'extlib/plugman/src'))
-$:.unshift(File.join(File.expand_path(File.dirname(base)), 'extlib/net-dns/lib'))
+$:.unshift(File.join(File.expand_path(File.dirname(base)), 'lib/net-dns/lib'))
 
 require 'core'
 require 'ui/cli/console'
