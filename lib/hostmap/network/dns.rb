95d8c003e3fbc5fb75c69dc5d0a07feb583f82d0
jekil <alessandro@tanasi.it>
2012-03-19 23:56:52 +0100

    Added dns request proxy

---
+++ lib/hostmap/network/dns.rb
@@ -0,0 +1,28 @@
+module Hostmap
+  module Network
+
+    #
+    # Net-dns adapter.
+    #
+    class Dns
+      def self.configure(dns=nil)
+        if dns
+          @@resolver = Net::DNS::Resolver.new(:nameserver => dns)
+        else
+          @@resolver = Net::DNS::Resolver.new
+        end
+        # Silence logger.
+        @@resolver.log_level = Net::DNS::UNKNOWN
+      end
+
+      def self.query(query, type=nil)
+        if type
+          @@resolver.query(query, type)
+        else
+          @@resolver.query(query)
+        end
+      end
+    end
+
+  end
+end
